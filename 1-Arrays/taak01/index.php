bcfcb835bbe8923b7918f2451b644454e0a08eb4
baszer <basfvega26@gmail.com>
2020-06-24 23:57:51 +0200

    ..

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$piet = array("naam"=>"Piet", "leeftijd"=>"37", "klas"=>"3B");
+$bruhemias = array("naam"=>"Bruhemias", "leeftijd"=>"34", "klas"=>"4B");
+$gerardius = array("naam"=>"Gerardius", "leeftijd"=>"31", "klas"=>"6B");
+
+
+echo $piet["naam"]."<br>"; 
+echo $piet["leeftijd"]."<br>"; 
+echo $piet["klas"]."<br>"; 
+
+
+?>
+
+
+</body>
+</html>
